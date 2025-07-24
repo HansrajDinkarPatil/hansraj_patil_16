44d53f4b174082bdf397d6d79de12834c438c260
Hansraj_patil_16 <hansrajpatil1611@gmail.com>
2025-07-25 00:41:59 +0530

    Add files via upload

---
+++ c programming/q3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+
+	int  number , lasttwodigit;
+	
+	printf("enter the number");
+	scanf("%d",&number);
+	
+	lasttwodigit=number%100;
+	
+	printf("last two digit are %d",lasttwodigit);
+
+	
+	
+}
